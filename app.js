80864167eb0a92423bd9a42fe76ac7d8b61dc6c8
AxelOP96 <axeelleguero@gmail.com>
2021-05-26 23:53:07 -0300

    update JS

---
+++ app.js
@@ -0,0 +1,5 @@
+document.body.style.backgroundColor = '#1cf';
+// let recuerdame = ['miguel', 'coco'];
+//          console.log(recuerdame);
+//         recuerdame.push('juana', 'rodrigo');
+//         console.log(recuerdame);
