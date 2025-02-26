b4b690f0780715e87209aab43e566f6303cb622f
Axel <axeelleguero@gmail.com>
2025-02-26 20:44:17 -0300

    menu hamburguesa final

--- hamburguer.js
+++ hamburguer.js
@@ -1,3 +1,18 @@
+export default function hamburgerMenu(panelBtn, panel, menuLink){
+    const d = document;
+    d.addEventListener("click", (e) =>{
+        if(e.target.matches(panelBtn) || e.target.matches(`${panelBtn} *`)){
+            d.querySelector(panel).classList.toggle("is-active");
+            d.querySelector(panelBtn).classList.toggle("is-active");
+        }
+
+        if(e.target.matches(menuLink)){
+            d.querySelector(panel).classList.remove("is-active");
+            d.querySelector(panelBtn).classList.remove("is-active");
+        }
+    });
+}
+/*
 const $hamburger = document.getElementById("hamburger");
 const $menu = document.querySelector('.d-none');
 const $title = document.getElementById("title");
@@ -6,10 +21,6 @@ const $li = document.querySelectorAll("li");
 $hamburger.addEventListener("click", () => {
     //$hamburger.classList.add("is-active");
     $menu.classList.toggle('d-none');
-    /*if($hamburger.classList.contains("is-active")){
-        $hamburger.classList.toggle("is-active");
-        $menu.classList.add("d-none");
-    }*/
     
 });
 
@@ -18,3 +29,5 @@ document.addEventListener("click", (e) =>{
         $title.innerHTML = e.target.className;
     }
 });
+*/
+
---
+++ index_dom.js
@@ -0,0 +1,6 @@
+import hamburgerMenu from "./hamburguer.js";
+/** */
+const d = document;
+d.addEventListener("DOMContentLoaded", (e) =>{
+    hamburgerMenu(".panel-btn", ".panel", ".menu a");
+})
