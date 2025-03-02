4c029a6330768135bd01bf68f69c13d6ebff0c64
Axel <axeelleguero@gmail.com>
2025-03-01 23:42:38 -0300

    Ejercicio final local storage

---
+++ 08DOM/index_dom.js
@@ -0,0 +1,27 @@
+import alarm from "./alarm.js";
+import reloj from "./clock.js";
+import finalCountdown from "./countdown.js";
+import hamburgerMenu from "./hamburguer.js";
+import guardarModo from "./local_storage.js";
+import changeMode from "./mode.js";
+import {atajos, move} from "./movement.js";
+import goTop from "./scroll.js";
+/** */
+const d = document;
+d.addEventListener("DOMContentLoaded", (e) =>{
+    hamburgerMenu(".panel-btn", ".panel", ".menu a");
+    finalCountdown(".countdown");
+    goTop(".top");
+    changeMode(".mode", ".body", ".sun", ".moon");
+    guardarModo(".sun" ,".moon", e);
+})
+d.addEventListener("DOMContentLoaded", (e) =>{
+    reloj(".start-c", ".stop-c", ".clock")
+})
+d.addEventListener("DOMContentLoaded", (e)=>{
+    alarm(".start-a", ".stop-a", ".sound");
+})
+d.addEventListener("keydown", (e) =>{
+    move(".circle", ".area", e)
+    atajos(e)
+});
